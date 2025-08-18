02ee91c01a68f28128d3e78f1a59b2c200d34a15
Kyla <kyla123ma@gmail.com>
2025-08-18 11:29:14 -0700

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,6 @@ const playfairDisplay = Playfair_Display({
 export const metadata: Metadata = {
   title: "Recipe Log",
   description: "Your personal recipe manager",
-  icons: {
-    icon: "/cupcake.png",
-  },
 };
 
 export default function RootLayout({
